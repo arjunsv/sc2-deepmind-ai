d6b485cc2bd893a18067bdab466dbcd8e3a68ccd
Arjun Vasudevan <arjunsv@berkeley.edu>
2018-07-15 17:06:16 -0700

    add opencv visualization

--- ArjBot.py
+++ ArjBot.py
@@ -1,44 +1,125 @@
 import sc2
+import random
+import cv2
+import numpy as np
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
+ CYBERNETICSCORE, STALKER, STARGATE, VOIDRAY
 
 class ArjBot(sc2.BotAI):
-	async def on_step(self, iteration):
-		await self.distribute_workers()
-		await self.build_workers()
-		await self.build_pylons()
-		await self.build_assimilator()
-		await self.expand
+    def __init__(self):
+        self.ITERATIONS_PER_MINUTE = 165
+        self.MAX_WORKERS = 50
 
-	async def build_workers(self):
-		for nexus in self.units(NEXUS).ready.noqueue:
-			if self.can_afford(PROBE):
-				await self.do(nexus.train(PROBE))
+    async def on_step(self, iteration):
+        self.iteration = iteration
 
-	async def build_pylons(self):
-		if self.supply_left < 5 and not self.already_pending(PYLON):
-			nexuses = self.units(NEXUS).ready
-			if nexuses.exists:
-				if self.can_afford(PYLON):
-					await self.build(PYLON, near=nexuses.first)
+        await self.intel()
+        await self.distribute_workers()
+        await self.build_workers()
+        await self.build_pylons()
+        await self.build_assimilators()
+        await self.expand()
+        await self.build_tech()
+        await self.build_army()
+        await self.attack()
 
-	async def build_assimilator(self):
-		for nexus in self.units(NEXUS).ready:
-            geysers = self.state.vespene_geyser.closer_than(20.0, nexus)
-            for geyser in geysers:
+    async def render_visualization(self):
+        game_data = np.zeros((self.game_info.map_size[1], self.game_info.map_size[0]), 3, np.uint8)
+        
+        for nexus in self.units(NEXUS):
+            nex_pos = nexus.position
+            cv2.circle(game_data, (int(nex_pos[0]), int(nex_pos[1])), 10, (0, 255, 0), -1)
+
+
+        flipped = cv2.flip(game_data, 0)
+        resized = cv2.resize(flipped, dsize=None, fx=2, fy=2)
+        cv2.imshow('Visualization', resized)
+        cv2.waitKey(1)
+
+    async def build_workers(self):
+        if (len(self.units(NEXUS)) * 16) > len(self.units(PROBE)) and len(self.units(PROBE)) < self.MAX_WORKERS:
+            for nexus in self.units(NEXUS).ready.noqueue:
+                if self.can_afford(PROBE):
+                    await self.do(nexus.train(PROBE))
+
+
+    async def build_pylons(self):
+        if self.supply_left < 5 and not self.already_pending(PYLON):
+            nexuses = self.units(NEXUS).ready
+            if nexuses.exists:
+                if self.can_afford(PYLON):
+                    await self.build(PYLON, near=nexuses.first)
+
+    async def build_assimilators(self):
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
+            for vaspene in vaspenes:
                 if not self.can_afford(ASSIMILATOR):
                     break
-
-                worker = self.select_build_worker(vg.position)
-                if not worker:
+                worker = self.select_build_worker(vaspene.position)
+                if worker is None:
                     break
+                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
 
-                if not self.units(ASSIMILATOR).closer_than(1.0, vg).exists:
-                    await self.do(worker.build(ASSIMILATOR, geyser))
+    async def expand(self):
+        if self.units(NEXUS).amount < (self.iteration / self.ITERATIONS_PER_MINUTE) and self.can_afford(NEXUS):
+            await self.expand_now()
+
+    async def build_tech(self):
+        #print(self.iteration / self.ITERATIONS_PER_MINUTE)
+        if self.units(PYLON).ready.exists:
+            pylon = self.units(PYLON).ready.random
+
+            if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
+                if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
+                    await self.build(CYBERNETICSCORE, near=pylon)
+
+            elif len(self.units(GATEWAY)) < 1:
+                if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
+                    await self.build(GATEWAY, near=pylon)
+
+            if self.units(CYBERNETICSCORE).ready.exists:
+                if len(self.units(STARGATE)) < ((self.iteration / self.ITERATIONS_PER_MINUTE)):
+                    if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
+                        await self.build(STARGATE, near=pylon)
+
+    async def build_army(self):
+        for gw in self.units(GATEWAY).ready.noqueue:
+            if not self.units(STALKER).amount > self.units(VOIDRAY).amount:
+                if self.can_afford(STALKER) and self.supply_left > 0:
+                    await self.do(gw.train(STALKER))
+
+        for sg in self.units(STARGATE).ready.noqueue:
+            if self.can_afford(VOIDRAY) and self.supply_left > 0:
+                await self.do(sg.train(VOIDRAY))
+
+    def find_target(self, state):
+        if self.known_enemy_units:
+            return random.choice(self.known_enemy_units)
+        elif self.known_enemy_structures:
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
+
+    async def attack(self):
+        # {UNIT: [n to fight, n to defend]}
+        attack_units = {VOIDRAY: [8, 3]}
+
+        for UNIT in attack_units:
+            if self.units(UNIT).amount > attack_units[UNIT][0] and self.units(UNIT).amount > attack_units[UNIT][1]:
+                for s in self.units(UNIT).idle:
+                    await self.do(s.attack(self.find_target(self.state)))
+
+            elif self.units(UNIT).amount > attack_units[UNIT][1]:
+                if self.known_enemy_units:
+                    for s in self.units(UNIT).idle:
+                        await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 
 run_game(maps.get("AbyssalReefLE"), [
-		Bot(Race.Protoss, ArjBot()),
-		Computer(Race.Terran, Difficulty.Easy)
-		], realtime=True)
+    Bot(Race.Protoss, ArjBot()),
+    Computer(Race.Terran, Difficulty.Hard)
+    ], realtime=False)
